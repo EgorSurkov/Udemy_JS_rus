c5281a56a2f7616894f669bbc5903f96a52b15e8
EgorSurkov <54619451+EgorSurkov@users.noreply.github.com>
2019-11-28 18:32:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+var test = 1;
+let second = 2;
+const pi = 3.14;
+
+
+
+
+var obj = {
+    name: "Sassy",
+    age: 19,
+    isWhore: true
+};
+
+
+
+ 
+alert (obj.name + " is a "  + obj.isWhore + " whore" + "now she is " + obj.age);
+
+confirm("Are u gay?");
