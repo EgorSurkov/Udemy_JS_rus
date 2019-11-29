68e3f659f825d4adee4d690602be38a4b7534115
EgorSurkov <54619451+EgorSurkov@users.noreply.github.com>
2019-11-29 11:26:20 +0300

    Add files via upload
    First homework

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 
-
-
-var test = 1;
-let second = 2;
-const pi = 3.14;
-
-
-
-
-var obj = {
-    name: "Sassy",
-    age: 19,
-    isWhore: true
-};
-
-
+ 
 
  
-alert (obj.name + " is a "  + obj.isWhore + " whore" + "now she is " + obj.age);
+var money = +prompt("Ваш бюджет на месяц?");
+var time =  prompt("Введите дату в формате YYYY-MM-DD");
 
-confirm("Are u gay?");
+var appData = {
+    budget : money,
+    timeData : time,
+    expenses : {expenditure,sizeExpenditure,expenditure1,sizeExpenditure1},
+    optionalExpenses : 0,
+    income : 0,
+    savings : false
+};
+
+var expenditure = prompt("Введите обязательную статью расходов в этом месяце");
+var sizeExpenditure = prompt("Во сколько это обойдется?");
+
+var expenditure1 = prompt("Введите обязательную статью расходов в этом месяце");
+var sizeExpenditure1 = prompt("Во сколько это обойдется?");
+
+var perDay = money/30;
+alert("Ваш бюджет на 1 день равен " + perDay + " рублей");
